67f5c0c5130ebfff31c217514dc2bc9645c43fcd
Alessandro Vieira <alessandro.vieira@aquare.la>
2021-03-01 07:32:36 -0300

    erase some create-react-app files. Nav and Banner components

---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <header className="header">
+      <div className="container">
+          <div className="row">
+              <div className="col-6">
+                <div className="header__content">
+                    <ul className="header__ul"></ul>
+                </div>
+              </div>
+              <div className="col-6">
+
+              </div>
+          </div>
+      </div>
+    </header>
+  );
+};
+
+export default Banner;
